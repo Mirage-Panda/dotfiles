797037a6ff73038a74fb1fe2b65d9e72d50aa250
Mirage-Panda <106939744+Mirage-Panda@users.noreply.github.com>
2024-09-28 17:52:47 +0800

    Added screenshots and NeoVim Config

---
+++ configs/nvim/lua/chadrc.lua
@@ -0,0 +1,23 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "catppuccin",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- Use nvdash on startup
+M.ui = {
+  nvdash = {
+    load_on_startup = true,
+  }
+}
+
+return M
