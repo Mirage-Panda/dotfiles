e6bc17fd09982da28d50221f0794c9e014f3a400
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-01-15 21:43:16 +0800

    Formatting

--- configs/sketchybar/settings.lua
+++ configs/sketchybar/settings.lua
@@ -4,19 +4,15 @@ return {
 
 	icons = "sf-symbols", -- alternatively available: NerdFont
 
-	-- This is a font configuration for SF Pro and SF Mono (installed manually)
-	font = require("helpers.default_font"),
-
-	-- Alternatively, this is a font config for JetBrainsMono Nerd Font
-	-- font = {
-	--   text = "JetBrainsMono Nerd Font", -- Used for text
-	--   numbers = "JetBrainsMono Nerd Font", -- Used for numbers
-	--   style_map = {
-	--     ["Regular"] = "Regular",
-	--     ["Semibold"] = "Medium",
-	--     ["Bold"] = "SemiBold",
-	--     ["Heavy"] = "Bold",
-	--     ["Black"] = "ExtraBold",
-	--   },
-	-- },
+	font = {
+		text = "SF Pro", -- Used for text
+		numbers = "SF Mono", -- Used for numbers
+		style_map = {
+			["Regular"] = "Regular",
+			["Semibold"] = "Semibold",
+			["Bold"] = "Bold",
+			["Heavy"] = "Heavy",
+			["Black"] = "Black",
+		},
+	},
 }
