f769eaa80ed481357a366e3e3bf587c641c50a72
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-01-28 20:52:06 +0000

    Use correct font

--- configs/sketchybar/items/spaces.lua
+++ configs/sketchybar/items/spaces.lua
@@ -10,9 +10,6 @@ for i = 1, 10, 1 do
 		space = i,
 		icon = {
 			font = { family = settings.font.numbers },
-			font = {
-				style = settings.font.style_map["Semibold"],
-			},
 			string = i,
 			padding_left = 15,
 			padding_right = 8,
