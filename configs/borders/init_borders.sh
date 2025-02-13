59e9cd4b8db6999b5f6a2ba9a0f2e80618f96455
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-02-13 17:10:21 +0800

    Simplified borders config

---
+++ configs/borders/init_borders.sh
@@ -0,0 +1,3 @@
+source $HOME/.config/borders/variables.sh
+
+borders width=$WIDTH active_color="gradient(top_left=0xee$MOCHA_LAVENDER,bottom_right=0xee$MOCHA_MAUVE)" inactive_color="gradient(top_left=0x88$MOCHA_OVERLAY2,bottom_right=0xdd$MOCHA_SURFACE2)" style=round hidpi=off
